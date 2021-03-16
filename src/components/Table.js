f82b4babccae6313f6d862bbe127a401ea90b568
SunPear <sunpear.sp@gmail.com>
2021-03-16 20:52:35 +0700

    remove meaningless colum

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,39 +1,37 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 export default class Table extends Component {
-  deleteItem = (id) => {
-    axios.delete('http://localhost:5000' + window.location.pathname + '/' + id)
-      .then(res => {
-        console.log(res.data)
-        alert(window.location.pathname.charAt(1).toUpperCase() + window.location.pathname.substr(2) + ' removed successfully.')
-        window.location.reload()
-      })
-      .catch(err => alert(err))
-  }
+  
   render() {
-    var key
-    if (Array.isArray(this.props.data) && this.props.data.length > 0 && typeof (this.props.data[0]) == "object") {
-      key = Object.keys(this.props.data[0])
+    var keys = []
+    if (this.props.data!==undefined&&Array.isArray(this.props.data) && this.props.data.length > 0 && typeof (this.props.data[0]) == "object") {
+      Object.keys(this.props.data[0]).map(item=>{
+        
+        if (this.props.data[0][item]===null||this.props.data[0][item]=='0'||Array.isArray(this.props.data[0][item])){
+          
+        }
+        else keys.push(item)
+      })
+      
       return (
         <table className="table">
           <thead className="thead-dark">
             <tr>
-              {Array.isArray(this.props.data) && typeof (this.props.data[0]) == "object" ?
-                Object.keys(this.props.data[0]).map(col => (
+              {
+                keys.map(col => (
                   <th scope="col" key={col}>{col.charAt(0).toUpperCase() + col.substr(1)}</th>
-                )) : ''}
+                )) }
             </tr>
           </thead>
           <tbody>
-            {Array.isArray(this.props.data) ? this.props.data.map(item => {
+            {this.props.data.map(item => {
               let result = []
-              result.push(<td scope='row'>{item[key[0]]}</td>)
-              for (let i = 1; i < key.length; i++) {
-                result.push(<td>{item[key[i]]}</td>)
+              result.push(<td scope='row'>{item[keys[0]]}</td>)
+              for (let i = 1; i < keys.length; i++) {
+                result.push(<td>{item[keys[i]]}</td>)
               }
-              return <tr key={item[key[0]]}>{result}</tr>
-            }) : ''}
+              return <tr key={item[keys[0]]}>{result}</tr>
+            })}
           </tbody>
         </table>
       )
